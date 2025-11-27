dd2d29b840c7163d99ad06689b7ecf43e9bf100b
Patrick <186724585+patrickbrouhard@users.noreply.github.com>
2025-11-27 18:50:38 +0100

    setup: Prettier, ESLint, etc

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+import globals from "globals";
+import prettier from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js", "**/*.jsx"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
+  prettier,
+];
--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
 const Pizza = (props) => {
-    return React.createElement('div', null,
-        [
-            React.createElement('h1', null, props.name),
-            React.createElement('p', null, props.description),
-        ]);
+  return React.createElement("div", null, [
+    React.createElement("h1", null, props.name),
+    React.createElement("p", null, props.description),
+  ]);
 };
 
 const App = () => {
-    return React.createElement(
-        'div',
-        null,
-        [
-            React.createElement('h1', null, 'Bienvenue chez Claudissimo Pizza!'),
-            React.createElement(Pizza, {
-                name: 'Pizza Quattro Formaggi',
-                description: 'Beaufort, Abondance, Reblochon et Tome des Bauges',
-            }),
-            React.createElement(Pizza, {
-                name: 'Pizza Racletta',
-                description: 'mozzarella, raclette fondue, charcuterie savoyarde (jambon cru, coppa), cornichons.',
-            }),
-            React.createElement(Pizza, {
-                name: 'Pizza Montagnardina',
-                description: 'tomme de Savoie, champignons, saucisse fumée, herbes de montagne.',
-            }),
-            React.createElement(Pizza, {
-                name: 'Diots e formaggio',
-                description: 'Diots (saucisses savoyardes), fromage fondu, oignons rouges.',
-            }),
-            React.createElement(Pizza, {
-                name: 'Pizza Margherita',
-                description: 'Base tomate, mozzarella, basilic frais.',
-            }),
-        ]
-    );
+  return React.createElement("div", null, [
+    React.createElement("h1", null, "Bienvenue chez Claudissimo Pizza!"),
+    React.createElement(Pizza, {
+      name: "Pizza Quattro Formaggi",
+      description: "Beaufort, Abondance, Reblochon et Tome des Bauges",
+    }),
+    React.createElement(Pizza, {
+      name: "Pizza Racletta",
+      description:
+        "mozzarella, raclette fondue, charcuterie savoyarde (jambon cru, coppa), cornichons.",
+    }),
+    React.createElement(Pizza, {
+      name: "Pizza Montagnardina",
+      description:
+        "tomme de Savoie, champignons, saucisse fumée, herbes de montagne.",
+    }),
+    React.createElement(Pizza, {
+      name: "Diots e formaggio",
+      description:
+        "Diots (saucisses savoyardes), fromage fondu, oignons rouges.",
+    }),
+    React.createElement(Pizza, {
+      name: "Pizza Margherita",
+      description: "Base tomate, mozzarella, basilic frais.",
+    }),
+  ]);
 };
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(React.createElement(App));
