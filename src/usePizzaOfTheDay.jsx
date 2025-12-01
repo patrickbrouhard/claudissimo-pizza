93a41dc18d317d6f5a62d18d03c7a7a844185e5c
Patrick <186724585+patrickbrouhard@users.noreply.github.com>
2025-12-01 16:36:10 +0100

    tests de custom hooks

--- src/usePizzaOfTheDay.jsx
+++ src/usePizzaOfTheDay.jsx
@@ -3,19 +3,19 @@ import { useState, useEffect, useDebugValue } from "react";
 // un custom hook est juste une fonction qui appelle d'autres fonction
 // pas de wrapper ni rien d'autre, c'est vraiment tout
 export const usePizzaOfTheDay = () => {
-    const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
+  const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
 
-    useDebugValue(pizzaOfTheDay ? `${pizzaOfTheDay.name}` : "Loading...");
+  useDebugValue(pizzaOfTheDay ? `${pizzaOfTheDay.name}` : "Loading...");
 
-    useEffect(() => {
-        async function fetchPizzaOfTheDay() {
-            const response = await fetch("/api/pizza-of-the-day");
-            const data = await response.json();
-            setPizzaOfTheDay(data);
-        }
+  useEffect(() => {
+    async function fetchPizzaOfTheDay() {
+      const response = await fetch("/api/pizza-of-the-day");
+      const data = await response.json();
+      setPizzaOfTheDay(data);
+    }
 
-        fetchPizzaOfTheDay();
-    }, []);
+    fetchPizzaOfTheDay();
+  }, []);
 
-    return pizzaOfTheDay;
+  return pizzaOfTheDay;
 };
